eb5f685268f3a5d33506f11ed8ab26ad7b0e1e93
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-12-25 00:39:21 +0530

    function api caching and revalidation

---
+++ 14_core_function_and_hooks/src/app/page.tsx
@@ -0,0 +1,18 @@
+export default async function Home() {
+  const timerResponse = await fetch("http://localhost:3000/api/timer" , {
+    cache: "force-cache",
+    next: { revalidate: 10, tags: ["timer"] } ,
+  })
+  const timerData = await timerResponse.json()
+
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center text-3xl">
+      <h1>Home</h1>
+      <br/>
+      <h2>Caching and Revalidation</h2>
+      <p>Time : {timerData.readable}</p>
+      <p>RequestId : {timerData.requestId}</p>
+      <br/>
+    </div>
+  );
+}
